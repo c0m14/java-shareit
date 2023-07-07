558a0ea1ceaa7c2a008e3af96fcd30d3801f070d
Nikita Komochkov <ascii2@yandex.ru>
2023-07-07 22:26:21 +0300

    fix: change validation to itemDto to accept null, but not empty

--- src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -2,6 +2,7 @@ package ru.practicum.shareit.item.dto;
 
 import lombok.Builder;
 import lombok.Data;
+import ru.practicum.shareit.util.validator.NullOrNotBlank;
 
 import javax.validation.constraints.Size;
 
@@ -13,10 +14,15 @@ import javax.validation.constraints.Size;
 public class ItemDto {
 
     private Long id;
+
+    @NullOrNotBlank
     @Size(max = 50)
     private String name;
+
+    @NullOrNotBlank
     @Size(max = 200)
     private String description;
+
     private Boolean available;
     private Long requestId;
 
