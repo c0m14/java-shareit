853897a9dc032a3ddec98a839cfd789ee7e8048b
Nikita Komochkov <ascii2@yandex.ru>
2023-07-07 21:07:57 +0300

    fix: change validation to userDto to accept null, but not empty

--- src/main/java/ru/practicum/shareit/util/validator/NullOrNotBlankValidator.java
+++ src/main/java/ru/practicum/shareit/util/validator/NullOrNotBlankValidator.java
@@ -1,2 +1,14 @@
-package ru.practicum.shareit.util.validator;public class NullOrNotBlankValidator {
+package ru.practicum.shareit.util.validator;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class NullOrNotBlankValidator implements ConstraintValidator<NullOrNotBlank, String> {
+
+    public void initialize(NullOrNotBlank parameters) {
+    }
+
+    public boolean isValid(String value, ConstraintValidatorContext constraintValidatorContext) {
+        return value == null || value.trim().length() > 0;
+    }
 }
